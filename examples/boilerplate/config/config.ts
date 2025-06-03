4b25215e737d8e7dc2c373f9f737395c1b9a33f2
xiaohuoni <xiaohuoni@dingtalk.com>
2025-06-03 14:29:26 +0800

    feat: umi@4.4.11

--- examples/boilerplate/config/config.ts
+++ examples/boilerplate/config/config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   appType: 'h5',
   // 这个值在 appts 中被 getKeepAlive 修改
   keepalive: [/./],
+  // presets: ['@kmijs/preset-bundler'],
+  // Enable Rspack
+  // rspack: {},
   mobileLayout: true,
   legacyBuild: false,
   // mainPath:'users',
